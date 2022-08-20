59a80ce705e7b29fafa3f5d16cd3357ab19b2d2e
Jasmin <ssongnari@gmail.com>
2022-08-20 14:36:45 -0400

    commit tree b93d336ffa546e2947802f27f5431f8dc1db7b32

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewPlantForm() {
+function NewPlantForm({onAddPlant}) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [price, setPrice] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    fetch('http://localhost:6001/plants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        image: image,
+        price: price
+      })
+    })
+    .then(r => r.json())
+    .then(newPlant => onAddPlant(newPlant))
+  }
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
-        <button type="submit">Add Plant</button>
+        <input onChange={e => setName(e.target.value)} value={name} type="text" name="name" placeholder="Plant name" />
+        <input onChange={e => setImage(e.target.value)} value={image} type="text" name="image" placeholder="Image URL" />
+        <input onChange={e => setPrice(parseFloat(e.target.value))} value={price} type="number" name="price" step="0.01" placeholder="Price" />
+        <button onClick={handleSubmit} type="submit">Add Plant</button>
       </form>
     </div>
   );
